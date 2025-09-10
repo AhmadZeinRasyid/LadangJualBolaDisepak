051a40fbcd12aab0dbab37b8e2a8bedd0eeef8c4
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-09-10 09:57:39 +0700

    added Product model

---
+++ main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    description = models.TextField("Gokilll")
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
